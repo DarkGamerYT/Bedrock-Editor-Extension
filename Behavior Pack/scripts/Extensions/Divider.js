14183d22804643748013edd4bcd19cbabe80b93a
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-05-27 16:46:56 +0100

    Add files via upload

---
+++ Behavior Pack/scripts/Extensions/Divider.js
@@ -0,0 +1,8 @@
+export default (uiSession) => {
+    uiSession.toolRail.addTool(
+        {
+            displayString: "Divider",
+            icon: "pack://textures/editor/divider.png?filtering=point",
+        },
+    );
+};
