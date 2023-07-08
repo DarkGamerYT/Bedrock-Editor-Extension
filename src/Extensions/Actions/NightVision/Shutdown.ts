c3ff86b0d6d53ba2eca6583011553fa746faa819
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-07-08 11:44:02 +0100

    Delete src directory

--- src/Extensions/Actions/NightVision/Shutdown.ts
+++
@@ -1,1 +0,0 @@
-export const Shutdown = (uiSession: import("@minecraft/server-editor").IPlayerUISession) => uiSession.log.debug( `Shutting down ${uiSession.extensionContext.extensionName} extension` );
